3398c7c20e96d032904553ab09b18694d54d90e8
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-07-08 16:47:48 +0200

    feat: Add front end

--- front-end/src/app/features/metrics/pages/metrics-dashboard/metrics-dashboard.ts
+++ front-end/src/app/features/metrics/pages/metrics-dashboard/metrics-dashboard.ts
@@ -1,29 +1,42 @@
 import {Component, inject, signal} from '@angular/core';
 import { CommonModule, DatePipe, NgClass } from '@angular/common';
-import {MetricsService} from '../../services/metricsService';
+import {MetricsService} from '../../services/metrics-service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-metrics-dashboard',
-  imports: [CommonModule, DatePipe, NgClass],
+  imports: [CommonModule, DatePipe, NgClass, ReactiveFormsModule],
   templateUrl: './metrics-dashboard.html'
 })
 export class MetricsDashboard {
 
   private metricService: MetricsService = inject(MetricsService);
+  private readonly formBuilder = inject(FormBuilder);
 
   isLoading = signal<boolean>(false);
   errorMessage = signal<string | null>(null);
   userMetrics = this.metricService.userMetrics;
 
+  searchForm!: FormGroup;
+
   ngOnInit(): void {
-    this.loadDashboardData();
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.searchForm = this.formBuilder.group({
+      projectKey: ['', [Validators.required, Validators.minLength(2)]],
+      startDate: ['', []],
+      endDate: ['', []],
+      granularity: ['', []],
+    });
   }
 
   loadDashboardData(): void {
     this.isLoading.set(true);
     this.errorMessage.set(null);
-
-    this.metricService.getUserMetrics(['SMSUP'], '2025-01-01', '2025-07-01')
+    const { projectKey: projectKeys, startDate, endDate } = this.searchForm.value;
+    this.metricService.getUserMetrics([projectKeys], startDate, endDate)
       .subscribe({
         next: () => {
           this.isLoading.set(false);
