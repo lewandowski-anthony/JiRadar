3230f590f05bbc5335ce39b74000119359c4483c
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-07-16 13:38:14 +0200

    fix: fix Docker image for cloud

--- front-end/docker/entrypoint.sh
+++ front-end/docker/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-echo "window.ENV = { JIRADAR_BACKEND_URL: \"${JIRADAR_BACKEND_URL}\" };" > /usr/share/nginx/html/config.js
+TARGET_DIR="/usr/share/nginx/html"
+
+if [ -n "$APP_BASE_PATH" ]; then
+  TARGET_DIR="/usr/share/nginx/html${APP_BASE_PATH}"
+  mkdir -p "$TARGET_DIR"
+  cp -r /usr/share/nginx/html/dist/* "$TARGET_DIR"/
+else
+  cp -r /usr/share/nginx/html/dist/* "$TARGET_DIR"/
+fi
+
+echo "window.ENV = { JIRADAR_BACKEND_URL: \"${JIRADAR_BACKEND_URL}\" };" > "${TARGET_DIR}/config.js"
 
 exec nginx -g "daemon off;"
