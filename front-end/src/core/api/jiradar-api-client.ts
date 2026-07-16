1fc97be926b07e1c3ba4f0beb9ce18fe524e0f9a
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-07-16 14:44:06 +0200

    fix: Fix API Url

--- front-end/src/core/api/jiradar-api-client.ts
+++ front-end/src/core/api/jiradar-api-client.ts
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { mapKeysToCamel } from '../utils/case-mapper';
 import { getCookie } from '../utils/cookies';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
-
+const BASE_URL = (window as any).ENV?.JIRADAR_BACKEND_URL || import.meta.env.VITE_API_URL || 'http://localhost:8080';
 export const apiClient = axios.create({
     baseURL: BASE_URL,
     headers: {
