07d1afc571c273874717da8a2f9410e469cc071f
LewandowskiAnthony <lewandowski.anthony.pro@gmail.com>
2026-07-27 09:49:02 +0200

    feat: Add test coverage - #JIRADAR-82

--- back-end/src/test/java/com/jiradar/jiradarback/config/CucumberSpringConfiguration.java
+++ back-end/src/test/java/com/jiradar/jiradarback/config/CucumberSpringConfiguration.java
@@ -3,11 +3,13 @@ package com.jiradar.jiradarback.config;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
+@Import(AiTestConfig.class)
 public class CucumberSpringConfiguration {
 }
